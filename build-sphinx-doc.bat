a8cc042248b1d28a767e81bf31eb20944df27ccc
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-11-06 23:18:46 +0900

    PATH を追加

--- build-sphinx-doc.bat
+++ build-sphinx-doc.bat
@@ -2,6 +2,8 @@ if exist sandboxdoc.zip del sandboxdoc.zip
 if exist build          rmdir /s /q build
 
 pip install sphinx  --user
+
+set PATH=%APPDATA%\Python;%PATH%
 call make.bat htmlhelp
 
 "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" build\htmlhelp\sandboxdoc.hhp
